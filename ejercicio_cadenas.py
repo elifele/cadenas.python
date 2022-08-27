2a7fd95438579a0d9c67f3197a14f8e9272a4c16
elibass <elibass28@gmail.com>
2022-08-26 23:15:02 -0400

    mi segundo commit

---
+++ ejercicio_cadenas.py
@@ -0,0 +1,27 @@
+#metodo split()
+fecha = "abdiel - mama - papa"
+print(fecha.split())
+
+#metodo find()
+temperatura = "Saturno tiene un temperatura en tiempo de dia de 170° celsius  "  
+print(temperatura.find("temperatura"))
+
+#metodo count()
+temperatura = "Saturno tiene un temperatura en tiempo de dia de 170° celsius  "  
+print(temperatura.count("temperatura"))
+
+#metodo upper()
+temperatura = "Saturno tiene un temperatura en tiempo de dia de 170° celsius  "  
+print(temperatura.upper())
+
+#metodo lower()
+temperatura = "Saturno tiene un temperatura en tiempo de dia de 170° celsius  "  
+print(temperatura.lower())
+
+#metodo f-strings
+a = "Hola"
+b = "Abdiel"
+
+resultado = (f"{a} espero que estes comodo {b} ")
+print(resultado)
+
